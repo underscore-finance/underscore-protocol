7a0185274d25a30e12324c58bac1e22770c984b1
Rocko-O-Rama <diogo.perillo@gmail.com>
2025-11-26 22:05:59 -0700

    Deploy Hatchery and LevgVaults

--- migrations/base-mainnet/v1.1/1000-LevgVaults.py
+++ migrations/base-mainnet/v1.1/1000-LevgVaults.py
@@ -5,9 +5,6 @@ from tests.constants import ZERO_ADDRESS
 def migrate(migration: Migration):
     hq = migration.get_address("UndyHq")
 
-    # execute after migration
-    return
-
     # vaults
     vault_registry = migration.get_contract("VaultRegistry")
     levg_vault_helper = migration.get_contract("LevgVaultHelper")
@@ -20,10 +17,10 @@ def migrate(migration: Migration):
         hq,  # _undyHq: address,
         migration.get_address("UndyUsd"),  # _collateralVaultToken: address,
         migration.blueprint.LEGO_IDS.UNDERSCORE,  # _collateralVaultTokenLegoId: uint256,
-        migration.blueprint.LEGO_IDS.RIPE,  # _collateralVaultTokenRipeVaultId: uint256,
+        5,  # _collateralVaultTokenRipeVaultId: uint256,
         migration.get_address("UndyUsd"),  # _leverageVaultToken: address,
         migration.blueprint.LEGO_IDS.UNDERSCORE,  # _leverageVaultTokenLegoId: uint256,
-        migration.blueprint.LEGO_IDS.RIPE,  # _leverageVaultTokenRipeVaultId: uint256,
+        5,  # _leverageVaultTokenRipeVaultId: uint256,
         migration.blueprint.TOKENS["USDC"],  # _usdc: address,
         migration.blueprint.TOKENS["GREEN"],  # _green: address,
         migration.blueprint.TOKENS["SAVINGS_GREEN"],  # _savingsGreen: address,
@@ -33,21 +30,22 @@ def migrate(migration: Migration):
         levg_vault_helper,  # _levgVaultHelper: address,
     )
     migration.execute(vault_registry.startAddNewAddressToRegistry, usdc_levg_vault.address, "Undy Levg USDC Vault")
-    assert migration.execute(vault_registry.confirmNewAddressToRegistry,
-                             usdc_levg_vault.address,
-                             True,  # isLeveragedVault
-                             False,  # shouldEnforceAllowlist
-                             [],  # doesn't matter for leverage vault
-                             0,  # maxDepositAmount (0 = unlimited)
-                             100_000_000_000,  # doesn't matter for leverage vault
-                             0,  # doesn't matter for leverage vault
-                             ZERO_ADDRESS,  # doesn't matter for leverage vault
-                             True,  # shouldAutoDeposit
-                             True,  # canDeposit
-                             True,  # canWithdraw
-                             False,  # isVaultOpsFrozen
-                             2_00,  # redemptionBuffer (2%)
-                             )
+    assert migration.execute(
+        vault_registry.confirmNewAddressToRegistry,
+        usdc_levg_vault.address,
+        True,  # isLeveragedVault
+        False,  # shouldEnforceAllowlist
+        [],  # doesn't matter for leverage vault
+        0,  # maxDepositAmount (0 = unlimited)
+        100_000_000_000,  # doesn't matter for leverage vault
+        0,  # doesn't matter for leverage vault
+        ZERO_ADDRESS,  # doesn't matter for leverage vault
+        True,  # shouldAutoDeposit
+        True,  # canDeposit
+        True,  # canWithdraw
+        False,  # isVaultOpsFrozen
+        2_00,  # redemptionBuffer (2%)
+    ) > 0
 
     cbbtc_levg_vault = migration.deploy(
         "LevgVault",
@@ -57,10 +55,10 @@ def migrate(migration: Migration):
         hq,  # _undyHq: address,
         migration.get_address("UndyBtc"),  # _collateralVaultToken: address,
         migration.blueprint.LEGO_IDS.UNDERSCORE,  # _collateralVaultTokenLegoId: uint256,
-        migration.blueprint.LEGO_IDS.RIPE,  # _collateralVaultTokenRipeVaultId: uint256,
+        5,  # _collateralVaultTokenRipeVaultId: uint256,
         migration.get_address("UndyUsd"),  # _leverageVaultToken: address,
         migration.blueprint.LEGO_IDS.UNDERSCORE,  # _leverageVaultTokenLegoId: uint256,
-        migration.blueprint.LEGO_IDS.RIPE,  # _leverageVaultTokenRipeVaultId: uint256,
+        5,  # _leverageVaultTokenRipeVaultId: uint256,
         migration.blueprint.TOKENS["USDC"],  # _usdc: address,
         migration.blueprint.TOKENS["GREEN"],  # _green: address,
         migration.blueprint.TOKENS["SAVINGS_GREEN"],  # _savingsGreen: address,
@@ -71,7 +69,8 @@ def migrate(migration: Migration):
     )
 
     migration.execute(vault_registry.startAddNewAddressToRegistry, cbbtc_levg_vault.address, "Undy Levg CBBTC Vault")
-    assert vault_registry.confirmNewAddressToRegistry(
+    assert migration.execute(
+        vault_registry.confirmNewAddressToRegistry,
         cbbtc_levg_vault.address,
         True,  # isLeveragedVault
         False,  # shouldEnforceAllowlist
